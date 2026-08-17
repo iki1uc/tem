a3406a346bed30f46f288e2882f6adf94d8d85c4
iki1uc <entertrainer@web.de>
2026-08-17 03:10:33 +0200

    Update KASSE.js

--- KASSE.js
+++ KASSE.js
@@ -1,1 +1,11 @@
+export const KASSE = {
+    name: "KASSE",
+    type: "TET",
+    role: "Abschluss",
+    vector: "end",
+    drive: "+T",
+    layer: "TEM",
+    diag: "N→W",
+    status: "aktiv"
+};
 
