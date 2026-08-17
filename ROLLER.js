c92c8ae7d0edcf9da3ca7f62785f89957926caae
iki1uc <entertrainer@web.de>
2026-08-17 03:10:21 +0200

    Update ROLLER.js

--- ROLLER.js
+++ ROLLER.js
@@ -1,1 +1,11 @@
+export const ROLLER = {
+    name: "ROLLER",
+    type: "TET",
+    role: "Transport",
+    vector: "move",
+    drive: "+T",
+    layer: "TEM",
+    diag: "O→N",
+    status: "aktiv"
+};
 
