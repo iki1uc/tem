2e0894bc4e55c365433d6785b7280725b85bdef5
iki1uc <entertrainer@web.de>
2026-08-17 03:07:45 +0200

    Update TEM6D.module.js

--- TEM6D.module.js
+++ TEM6D.module.js
@@ -1,21 +1,5 @@
-export const T6D = {
-
-    // 6D Zeitvektor
-    t: [0, 0, 0],   // t1 = ALT, t2 = GHOST, t3 = REAL
-
-    // Zeitfluss in 6 Dimensionen
-    flow(v, a, g){
-        const ε = 0.000001; // Kopplungsparameter (physikalisch erlaubt)
-
-        this.t[0] += v * ε;        // ALT-Zeit
-        this.t[1] += a * ε;        // GHOST-Zeit
-        this.t[2] += g * ε;        // REAL-Zeit
-
-        return this.t;
-    },
-
-    // Projektion zurück in 4D (iki1uc-Hauptzeit)
-    project4D(){
-        return this.t[2]; // REAL ist deine Hauptzeit
-    }
+export const TEM6E = {
+    TET: ["ESSO", "ROLLER", "KASSE"],
+    RIZ: ["DOOR", "DOO", "SHELL"],
+    all: ["ESSO", "ROLLER", "KASSE", "DOOR", "DOO", "SHELL"]
 };
