25916808bd14ba01b718cf9f6d99cb1a4679f617
iki1uc <entertrainer@web.de>
2026-08-17 02:57:10 +0200

    Create YxY.js

---
+++ YxY.js
@@ -0,0 +1,14 @@
+ESSO, ROLLER, KASSE = TET (Bewegung)
+DOOR, DOO, SHELL = RIZ (Gegenbewegung)
+
+TET = +T
+RIZ = -T
+
+6E = 3E TET + 3E RIZ
+Diese 6E fallen auf TEM (diagonal)
+
+SYS oben = 3E
+TEM mitte = 6E
+SYS unten = 9E
+
+(((SYS3))) TEM6 (((SYS9)))
