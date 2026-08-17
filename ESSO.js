9f28029245d6a3d32b0aa86f40c60f337438492d
iki1uc <entertrainer@web.de>
2026-08-17 03:10:08 +0200

    Update ESSO.js

--- ESSO.js
+++ ESSO.js
@@ -1,1 +1,11 @@
+export const ESSO = {
+    name: "ESSO",
+    type: "TET",
+    role: "Startimpuls",
+    vector: "start",
+    drive: "+T",
+    layer: "TEM",
+    diag: "S→O",
+    status: "aktiv"
+};
 
